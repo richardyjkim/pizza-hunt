9bc8c406a18d5a451d016e58a5506104793b8628
Richard <richardyjkim@gmail.com>
2021-05-27 16:26:34 -0700

    add replies contructed

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
 const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
