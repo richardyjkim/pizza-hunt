1277a1952d80d7ca4fc9dc7a5f4036e544d202a7
Richard <richardyjkim@gmail.com>
2021-05-31 15:25:55 -0700

    validation constructed

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -24,7 +24,7 @@ const commentController = {
 
   // add reply to comment
   addReply({ params, body }, res) {
-    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true, runValidators: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
@@ -45,7 +45,9 @@ const commentController = {
         return Pizza.findOneAndUpdate(
           { _id: params.pizzaId },
           { $pull: { comments: params.commentId } },
-          { new: true }
+          {
+            new: true, runValidators: true
+          }
         );
       })
       .then(dbPizzaData => {
